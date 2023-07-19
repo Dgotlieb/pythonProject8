f65d7a51064d17c13d8ca4dc238e94011658e5d4
dgotlieb <dgotlieb8@gmail.com>
2023-07-19 20:11:38 +0300

    change test

--- second.py
+++ second.py
@@ -1,1 +1,26 @@
-print(3)
+# class Shark():
+#     age = 1
+#     name = "samy"
+#     adresses = ["home", "work"]
+#
+# sammy1 = Shark()
+# sammy2 = Shark()
+#
+# sharks = [sammy1, sammy2]
+#
+# for shark in sharks:
+#     print(shark.name)
+#     for address in shark.adresses:
+#         print(address)
+
+x = 1
+y = 2
+z = 3
+
+my_list = []
+my_list.append(x)
+my_list.append(y)
+my_list.append(z)
+
+for item in my_list:
+    print(item)
